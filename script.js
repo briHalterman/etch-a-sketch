2600f4147c77091ac2dc816008bd8040ab25e7d2
Brittany Halterman <halterma@gmail.com>
2025-04-23 17:18:20 -0400

    Reset sketchWindow when resized
    - Add and event listener for window resize
    - Recalculate and regenerate sketchWindow

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const sketchWindow = document.getElementById("sketchWindow");
-const sketchWindowWidth = sketchWindow.clientWidth;
 let gridSize = 16; // number of squares per row or column
-const pixelCount = gridSize * gridSize;
-const pixelLength = sketchWindowWidth / gridSize;
 
-for (i = 0; i < pixelCount; i++) {
-  const pixel = document.createElement("div");
-  pixel.style.width = `${pixelLength}px`;
-  pixel.style.height = `${pixelLength}px`;
-  pixel.style.border = "1px solid black";
-  pixel.classList.add("pixel");
-  sketchWindow.appendChild(pixel);
-}
+const createEtchASketch = () => {
+  const sketchWindow = document.getElementById("sketchWindow");
+  const sketchWindowWidth = sketchWindow.clientWidth;
+  const pixelCount = gridSize * gridSize;
+  const pixelLength = sketchWindowWidth / gridSize;
+
+  sketchWindow.replaceChildren();
+
+  for (i = 0; i < pixelCount; i++) {
+    const pixel = document.createElement("div");
+    pixel.style.width = `${pixelLength}px`;
+    pixel.style.height = `${pixelLength}px`;
+    pixel.classList.add("pixel");
+    sketchWindow.appendChild(pixel);
+
+    pixel.addEventListener("mouseenter", () => {
+      pixel.style.backgroundColor = "#2E242D";
+    });
+  }
+};
+
+createEtchASketch();
+
+window.addEventListener("resize", () => {
+  createEtchASketch();
+});
